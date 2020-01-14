38508a8010a17d41a57526a8a8c7f9f1ad98d93e
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-01-14 21:02:00 +0100

    :bug: Fixed sides detection algorithm and started adding it to crop_image function

--- matlab_dir/crop_image.m
+++ matlab_dir/crop_image.m
@@ -1,123 +1,128 @@
 function out=crop_image(image)
     im = equalize_image(im2double(image));
-    % use a threshold on CR channel to get a mask of the box
-    ycbcr = rgb2ycbcr(im);
-    CR = ycbcr(:,:,3);
-    T = 100/255;
-    mask1 = CR < T;
-    % remove salt&pepper on mask
-    mask1 = medfilt2(mask1);
-    % use a threshold on BLUE channel to get a mask of the box
-    B = im(:,:,3);
-    T = 30/255;
-    mask2 = B < T;
-    % remove salt&pepper on mask
-    mask2 = medfilt2(mask2, [10, 10]);
-    se = strel("diamond", floor(min(size(mask2)) / 100));
-    mask2 = imclose(mask2, se);
-    % get the largest pixel area
-    mask2 = bwareafilt(mask2, 1);
-    % mix masks
-    mask = or(mask1,mask2);
-    % close mask perimeter
-    mask = imclose(mask, se);
-    % fill mask holes
-    mask = imfill(mask, 'holes');
-    % get the largest pixel area
-    mask = bwareafilt(mask, 1);
-    corners = find_corners(mask);
-    if valid_corners(corners)
-        %view_corners(im, corners);
-        % project the box to fill the image rectangle
-        d_mask = size(mask);
-        corners = [corners(:,1)./d_mask(1), corners(:,2)./d_mask(2)];
-        p_corners = [0,0; 1,0; 1,1; 0,1];
-        tform = maketform('projective', fliplr(corners), p_corners);
-        udata = [0 1];  vdata = [0 1];
-        [new_cropped, ~, ~] = imtransform(im, tform, 'bicubic', 'udata', udata, 'vdata', vdata, 'size', size(im), 'fill', 0);
-        [new_mask, ~, ~] = imtransform(mask, tform, 'bicubic', 'udata', udata, 'vdata', vdata, 'size', size(mask), 'fill', 0);
-        cropped = crop_rest_mask(new_cropped, new_mask);
+    sides = find_sides(im);
+    if length(sides) == 4
+        % TODO: Find corners using lines intersection
     else
-        % crop out the rest of the image
-        cropped = crop_rest_mask(im, mask);
-        cropped_mask = crop_rest_mask(mask, mask);
-        % if necessary rotate the image to get the long side of the box
-        % horizontally aligned
+        % use a threshold on CR channel to get a mask of the box
+        ycbcr = rgb2ycbcr(im);
+        CR = ycbcr(:,:,3);
+        T = 100/255;
+        mask1 = CR < T;
+        % remove salt&pepper on mask
+        mask1 = medfilt2(mask1);
+        % use a threshold on BLUE channel to get a mask of the box
+        B = im(:,:,3);
+        T = 30/255;
+        mask2 = B < T;
+        % remove salt&pepper on mask
+        mask2 = medfilt2(mask2, [10, 10]);
+        se = strel("diamond", floor(min(size(mask2)) / 100));
+        mask2 = imclose(mask2, se);
+        % get the largest pixel area
+        mask2 = bwareafilt(mask2, 1);
+        % mix masks
+        mask = or(mask1,mask2);
+        % close mask perimeter
+        mask = imclose(mask, se);
+        % fill mask holes
+        mask = imfill(mask, 'holes');
+        % get the largest pixel area
+        mask = bwareafilt(mask, 1);
+        corners = find_corners(mask);
+        if valid_corners(corners)
+            %view_corners(im, corners);
+            % project the box to fill the image rectangle
+            d_mask = size(mask);
+            corners = [corners(:,1)./d_mask(1), corners(:,2)./d_mask(2)];
+            p_corners = [0,0; 1,0; 1,1; 0,1];
+            tform = maketform('projective', fliplr(corners), p_corners);
+            udata = [0 1];  vdata = [0 1];
+            [new_cropped, ~, ~] = imtransform(im, tform, 'bicubic', 'udata', udata, 'vdata', vdata, 'size', size(im), 'fill', 0);
+            [new_mask, ~, ~] = imtransform(mask, tform, 'bicubic', 'udata', udata, 'vdata', vdata, 'size', size(mask), 'fill', 0);
+            cropped = crop_rest_mask(new_cropped, new_mask);
+        else
+            % crop out the rest of the image
+            cropped = crop_rest_mask(im, mask);
+            cropped_mask = crop_rest_mask(mask, mask);
+            % if necessary rotate the image to get the long side of the box
+            % horizontally aligned
+            [rows, columns, ~] = size(cropped);
+            if rows > columns
+                cropped = imrotate(cropped, 90);
+                cropped_mask = imrotate(cropped_mask, 90);
+                [rows, columns, ~] = size(cropped);
+            end
+            % get the bounds of the mask
+            boundaries = bwboundaries(cropped_mask);
+            x = boundaries{1}(:, 2);
+            y = boundaries{1}(:, 1);
+            % get lines on top side
+            top_pt = [];
+            for i=1:rows
+                for j=1:columns
+                    if cropped_mask(i, j) == 1
+                        top_pt = j;
+                        break;
+                    end
+                end
+                if not(isempty(top_pt))
+                    break;
+                end
+            end
+            side_elements = y < rows/2 & x < top_pt;
+            top_side_x = x(side_elements);
+            top_side_y = y(side_elements);
+            % get lines on bottom side
+            bottom_pt = [];
+            for i=rows:-1:1
+                for j=columns:-1:1
+                    if cropped_mask(i, j) == 1
+                        bottom_pt = j;
+                        break;
+                    end
+                end
+                if not(isempty(bottom_pt))
+                    break;
+                end
+            end
+            side_elements = y > rows/2 & x > bottom_pt;
+            bottom_side_x = x(side_elements);
+            bottom_side_y = y(side_elements);
+            %{
+            % show the sides
+            imshow(cropped_mask, []);
+            axis on;
+            hold on;
+            plot(top_side_x, top_side_y, 'r-', 'LineWidth', 3);
+            plot(bottom_side_x, bottom_side_y, 'r-', 'LineWidth', 3);
+            %}
+            % fit line to get the angle
+            top_coeffs = polyfit(top_side_x, top_side_y, 1);
+            bottom_coeffs = polyfit(bottom_side_x, bottom_side_y, 1);
+            % get the top angle
+            mean_slope = mean([top_coeffs(1), bottom_coeffs(1)]);
+            angle = atand(mean_slope);
+            % rotate the mask and the image to get the top side aligned
+            r_mask = imrotate(mask, angle);
+            r_im = imrotate(im, angle);
+            % crop out all the rest of the image
+            cropped = crop_rest_mask(r_im, r_mask);
+        end
+        % rotate the image in landscape
         [rows, columns, ~] = size(cropped);
         if rows > columns
             cropped = imrotate(cropped, 90);
-            cropped_mask = imrotate(cropped_mask, 90);
-            [rows, columns, ~] = size(cropped);
         end
-        % get the bounds of the mask
-        boundaries = bwboundaries(cropped_mask);
-        x = boundaries{1}(:, 2);
-        y = boundaries{1}(:, 1);
-        % get lines on top side
-        top_pt = [];
-        for i=1:rows
-            for j=1:columns
-                if cropped_mask(i, j) == 1
-                    top_pt = j;
-                    break;
-                end
-            end
-            if not(isempty(top_pt))
-                break;
-            end
+        % flip up/down the image if the white ferreros are on the bottom
+        [rows, ~, ~] = size(cropped);
+        bw = rgb2gray(cropped);
+        mask = bw > 0.5;
+        bottom_half_count = sum(sum(mask(floor(rows/2):rows, :)));
+        top_half_count = sum(sum(mask(1:floor(rows/2), :)));
+        if bottom_half_count > top_half_count
+            cropped = flipud(cropped);
         end
-        side_elements = y < rows/2 & x < top_pt;
-        top_side_x = x(side_elements);
-        top_side_y = y(side_elements);
-        % get lines on bottom side
-        bottom_pt = [];
-        for i=rows:-1:1
-            for j=columns:-1:1
-                if cropped_mask(i, j) == 1
-                    bottom_pt = j;
-                    break;
-                end
-            end
-            if not(isempty(bottom_pt))
-                break;
-            end
-        end
-        side_elements = y > rows/2 & x > bottom_pt;
-        bottom_side_x = x(side_elements);
-        bottom_side_y = y(side_elements);
-        %{
-        % show the sides
-        imshow(cropped_mask, []);
-        axis on;
-        hold on;
-        plot(top_side_x, top_side_y, 'r-', 'LineWidth', 3);
-        plot(bottom_side_x, bottom_side_y, 'r-', 'LineWidth', 3);
-        %}
-        % fit line to get the angle
-        top_coeffs = polyfit(top_side_x, top_side_y, 1);
-        bottom_coeffs = polyfit(bottom_side_x, bottom_side_y, 1);
-        % get the top angle
-        mean_slope = mean([top_coeffs(1), bottom_coeffs(1)]);
-        angle = atand(mean_slope);
-        % rotate the mask and the image to get the top side aligned
-        r_mask = imrotate(mask, angle);
-        r_im = imrotate(im, angle);
-        % crop out all the rest of the image
-        cropped = crop_rest_mask(r_im, r_mask);
+        out.image = cropped;
     end
-    % rotate the image in landscape
-    [rows, columns, ~] = size(cropped);
-    if rows > columns
-        cropped = imrotate(cropped, 90);
-    end
-    % flip up/down the image if the white ferreros are on the bottom
-    [rows, ~, ~] = size(cropped);
-    bw = rgb2gray(cropped);
-    mask = bw > 0.5;
-    bottom_half_count = sum(sum(mask(floor(rows/2):rows, :)));
-    top_half_count = sum(sum(mask(1:floor(rows/2), :)));
-    if bottom_half_count > top_half_count
-        cropped = flipud(cropped);
-    end
-    out.image = cropped;
 end
--- matlab_dir/find_sides.m
+++ matlab_dir/find_sides.m
@@ -1,30 +1,35 @@
 function out=find_sides(equalized_image)
-    % use a threshold on CR channel to get a mask of the box
+    % use a threshold to get a mask with the box sides
     ycbcr = rgb2ycbcr(equalized_image);
     CR = ycbcr(:,:,3);
     T = 110/255;
     mask1 = CR < T;
     mask1 = medfilt2(mask1);
+    % process mask edges and detect lines with hough
     BW = edge(mask1,'canny');
     [H,theta,rho] = hough(BW);
     P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
     lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
-    new_lines = [];
+    %{
+    % uncomment to plot all lines
     figure, imshow(equalized_image), hold on;
     for i = 1:length(lines)
-       
        xy = [lines(i).point1; lines(i).point2];
        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
        plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
        plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-       
-       % Determine the endpoints of the longest line segment
+    end
+    %}
+    % get only the lines longer then 50 pixels
+    new_lines = [];
+    for i = 1:length(lines)
        len = norm(lines(i).point1 - lines(i).point2);
        if len > 50
            new_lines = [new_lines; lines(i)];
        end
     end
     lines = new_lines;
+    % unify close lines with same angle
     new_lines = [];
     for i = 1:length(lines)
         found = 0;
@@ -40,17 +45,44 @@ function out=find_sides(equalized_image)
         end
     end
     lines = new_lines;
+    % remove extra lines if there are more than 4
     if length(lines) > 4
-        new_lines = [lines(1); lines(2); lines(3); lines(4)];
-        for i = 5:length(lines)
+        new_lines = [];
+        % find 4 starting lines
+        for i = 1:length(lines)
+            count = 0;
             for j = 1:length(new_lines)
                 delta_theta = abs(new_lines(j).theta - lines(i).theta);
-                delta_rho = abs(new_lines(j).rho - lines(i).rho);
-                if and(delta_theta < 5, delta_rho < 100)
-                    if lines(i).rho > new_lines(j).rho
-                        new_lines(j) = lines(i);
-                    end
+                if delta_theta < 5
+                    count = count + 1;
+                end
+            end
+            if count < 2
+                new_lines = [new_lines; lines(i)];
+            end
+            if length(new_lines) == 4
+                break;
+            end
+        end
+        % check if there are better lines in the others
+        for i = 1:length(lines)
+            there_is = false;
+            for j = 1:length(new_lines)
+                if and(new_lines(j).point1 == lines(i).point1, new_lines(j).point2 == lines(i).point2)
+                    there_is = true;
                     break;
+                end
+            end
+            if ~there_is
+                for j = 1:length(new_lines)
+                    delta_theta = abs(new_lines(j).theta - lines(i).theta);
+                    delta_rho = abs(new_lines(j).rho - lines(i).rho);
+                    if and(delta_theta < 5, delta_rho < 100)
+                        if lines(i).rho > new_lines(j).rho
+                            new_lines(j) = lines(i);
+                        end
+                        break;
+                    end
                 end
             end
         end
