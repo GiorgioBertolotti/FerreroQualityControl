41a4811155d763473c3db0270722bfcff5229e31
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-01-26 17:40:51 +0100

    :bug: Improved algorithm to find sides

--- matlab_dir/find_sides.m
+++ matlab_dir/find_sides.m
@@ -7,9 +7,9 @@ function out=find_sides(equalized_image)
     mask1 = medfilt2(mask1);
     % process mask edges and detect lines with hough
     BW = edge(mask1,'canny');
-    [H,theta,rho] = hough(BW);
-    P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
-    lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',7);
+    [H,T,R] = hough(BW);
+    P = houghpeaks(H,6,'threshold',ceil(0.3*max(H(:))));
+    lines = houghlines(BW,T,R,P,'FillGap',5,'MinLength',7);
     %{
     % uncomment to plot all lines
     figure, imshow(equalized_image), hold on;
@@ -47,46 +47,90 @@ function out=find_sides(equalized_image)
     lines = new_lines;
     % remove extra lines if there are more than 4
     if length(lines) > 4
-        new_lines = [];
-        % find 4 starting lines
+        similar_thetas = {};
+        % group the lines based on similar angles
         for i = 1:length(lines)
-            count = 0;
-            for j = 1:length(new_lines)
-                delta_theta = abs(new_lines(j).theta - lines(i).theta);
-                if delta_theta < 5
-                    count = count + 1;
-                end
-            end
-            if count < 2
-                new_lines = [new_lines; lines(i)];
-            end
-            if length(new_lines) == 4
-                break;
-            end
-        end
-        % check if there are better lines in the others
-        for i = 1:length(lines)
-            there_is = false;
-            for j = 1:length(new_lines)
-                if and(new_lines(j).point1 == lines(i).point1, new_lines(j).point2 == lines(i).point2)
-                    there_is = true;
-                    break;
-                end
-            end
-            if ~there_is
-                for j = 1:length(new_lines)
-                    delta_theta = abs(new_lines(j).theta - lines(i).theta);
-                    delta_rho = abs(new_lines(j).rho - lines(i).rho);
-                    if and(delta_theta < 5, delta_rho < 100)
-                        if lines(i).rho > new_lines(j).rho
-                            new_lines(j) = lines(i);
-                        end
+            if isempty(similar_thetas)
+                similar_thetas = {[lines(i)]};
+            else
+                found = 0;
+                for j = 1:length(similar_thetas)
+                    delta_theta = abs(similar_thetas{j}(1).theta - lines(i).theta);
+                    if delta_theta < 15
+                        similar_thetas{j} = [similar_thetas{j}, lines(i)];
+                        found = 1;
                         break;
                     end
                 end
+                if found == 0
+                    similar_thetas{end+1} = [lines(i)];
+                end
             end
         end
-        lines = new_lines;
+        % if there are more than two lines with similar angles pick the
+        % most external two
+        for i = 1:length(similar_thetas)
+            if length(similar_thetas{i}) > 2
+                min_rho = similar_thetas{i}(1);
+                max_rho = similar_thetas{i}(1);
+                for j = 2:length(similar_thetas{i})
+                    if similar_thetas{i}(j).rho < min_rho.rho
+                        min_rho = similar_thetas{i}(j);
+                    end
+                    if similar_thetas{i}(j).rho > max_rho.rho
+                        max_rho = similar_thetas{i}(j);
+                    end
+                end
+                similar_thetas{i} = [min_rho, max_rho];
+            end
+        end
+        % add back lines to main array
+        lines = [];
+        for i = 1:length(similar_thetas)
+            for j = 1:length(similar_thetas{i})
+                lines = [lines; similar_thetas{i}(j)];
+            end
+        end
+        % if there are still more than 4 lines
+        if length(lines) > 4
+            % find 4 starting lines
+            for i = 1:length(lines)
+                count = 0;
+                for j = 1:length(new_lines)
+                    delta_theta = abs(new_lines(j).theta - lines(i).theta);
+                    if delta_theta < 5
+                        count = count + 1;
+                    end
+                end
+                if count < 2
+                    new_lines = [new_lines; lines(i)];
+                end
+                if length(new_lines) == 4
+                    break;
+                end
+            end
+            % check if there are better lines in the others
+            for i = 1:length(lines)
+                there_is = false;
+                for j = 1:length(new_lines)
+                    if and(new_lines(j).point1 == lines(i).point1, new_lines(j).point2 == lines(i).point2)
+                        there_is = true;
+                        break;
+                    end
+                end
+                if ~there_is
+                    for j = 1:length(new_lines)
+                        delta_theta = abs(new_lines(j).theta - lines(i).theta);
+                        delta_rho = abs(new_lines(j).rho - lines(i).rho);
+                        if and(delta_theta < 5, delta_rho > 100)
+                            new_lines(j) = lines(i);
+                            break;
+                        end
+                    end
+                end
+            end
+            lines = new_lines;
+        end
     end
     out = lines;
 end
